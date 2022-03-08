ab8f37f054a0f121260cd2165cf16f82ce9344a1
Alexandra Sivira <kerensivira@gmail.com>
2022-03-08 13:28:39 -0500

    jh

--- Frontend/index.js
+++ Frontend/index.js
@@ -40,8 +40,6 @@ container_DATA.innerHTML +=`
         <img src="${imagen}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${product}</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
 
